c7b13553f954930d9c9e7243a0a7924fa4c6dd84
dexitional <ebenezerkb.ackah@gmail.com>
2026-05-23 16:50:05 +0000

    fixed bundler

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
-
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
-import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
-const config = defineConfig({
-  resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+// const config = defineConfig({
+//   resolve: { tsconfigPaths: true },
+//   plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+// })
+
+export default defineConfig({
+  plugins: [
+    nitro({
+      preset: 'node-server', // Or 'vercel', 'cloudflare-workers', etc.
+    }),
+    tanstackStart(), devtools(), tailwindcss(), 
+  ],
 })
-
-export default config
